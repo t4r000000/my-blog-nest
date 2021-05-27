4cf7d78abe9464f21b35be8a241c0b262174e031
ninginx <publictaro@gmail.com>
2021-05-27 20:31:53 +0900

    Log Output Completed

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,34 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
-import { TweetService } from './tweet.service';
+import { Controller } from "@nestjs/common";
+import { Cron, CronExpression } from "@nestjs/schedule";
+import { TweetService } from "./tweet.service";
 
-@Controller('tweet')
+@Controller("tweet")
 export class TweetController {
   constructor(private readonly tweetService: TweetService) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
-    name: 'summarizeTweet',
-    timeZone: 'Asia/Tokyo',
+    name: "summarizeTweet",
+    timeZone: "Asia/Tokyo",
   })
   async saveMonthlySummary(): Promise<string> {
     return new Promise(() => {
       this.tweetService
         .saveMonthlySummary()
         .then(() => {
-          console.log('implemented');
-        })
-        //Fix: Slack 通知飛ばすべべ？
-        .catch((error) => console.error(error));
-    });
-  }
-
-  @Get()
-  async saveSummaryByRequest(): Promise<string> {
-    return new Promise(() => {
-      this.tweetService
-        .saveMonthlySummary()
-        .then(() => {
-          console.log('implemented');
+          console.log(new Date(), "Tweet Summarized Completed");
         })
         //Fix: Slack 通知飛ばすべべ？
         .catch((error) => console.error(error));

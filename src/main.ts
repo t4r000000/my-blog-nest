4e5fc8750dd5a8814290bcbea55575d0680583fc
ninginx <publictaro@gmail.com>
2021-05-28 22:55:39 +0000

    cors config added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'localhost:3000',
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,

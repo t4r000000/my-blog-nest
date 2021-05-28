c8c0ebb57d9104d0292049de1a0395ef4f6e5e2e
ninginx <publictaro@gmail.com>
2021-05-28 22:48:39 +0000

    cors config added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   const server = await app.listen(8080);
   server.setTimeout(120000); //timeout 2min
 };
